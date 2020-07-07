a60514e73ad06284522b715d8fb2bef002f84263
Keracy <fazzzle.de@gmail.com>
2020-07-07 18:52:05 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Todos from "./Todos";
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" },
+    ],
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => todo.id !== id);
+    this.setState({
+      todos: todos,
+    });
+  };
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos deleteTodo={this.deleteTodo} todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
